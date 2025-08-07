5863deb92927013997103c73c8843d86744386a4
Imran <lambda1488@gmail.com>
2025-08-07 09:35:27 +0300

    Update merge-conflict-resolving

--- merge-conflict-resolving.py
+++ merge-conflict-resolving.py
@@ -1,8 +1,2 @@
-import time
-import numpy as np
-import pandas as pd
-
-def merge_conflict_resolving():
-    for i in range(10):
-        time.sleep(1)
-        print("merge conflict resolving")
+print("merge conflict resolving")
+print("1")
